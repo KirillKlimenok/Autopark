2ab99b5087807636d7918d4f2616c390e3fb3874
ivan.mamai00@mail.ru <250219As>
2021-06-18 23:56:57 +0300

    done level 2

--- src/dercochenko/com/Main.java
+++ src/dercochenko/com/Main.java
@@ -1,8 +1,60 @@
 package dercochenko.com;
 
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        VehicleType[] types = new VehicleType[]{
+                new VehicleType("Bus", 1.2),
+                new VehicleType("Car", 1),
+                new VehicleType("Rink", 1.5),
+                new VehicleType("Tractor", 1.2)};
+
+        display(types);
+
+        //set tax coefficient in last object
+        types[types.length - 1].setTaxCoefficient(1.3);
+
+        display(types);
+
+        VehicleType v = types[0];
+
+        for (VehicleType t :
+                types) {
+            if (t.getTaxCoefficient() > v.getTaxCoefficient()) {
+                v = t;
+            }
+        }
+
+        System.out.println(v);
+
+        double averageTaxCoefficient = 0;
+        for (VehicleType t :
+                types) {
+            averageTaxCoefficient += t.getTaxCoefficient();
+        }
+
+        System.out.println(averageTaxCoefficient / types.length);
+
+        for (VehicleType t :
+                types) {
+            if (t.getTaxCoefficient() > v.getTaxCoefficient()) {
+                v = t;
+            }
+
+            averageTaxCoefficient += t.getTaxCoefficient();
+        }
+
+        System.out.println(v + "\n" + averageTaxCoefficient);
+
     }
+
+    public static void display(VehicleType[] types) {
+        for (VehicleType e :
+                types) {
+            System.out.println(e);
+        }
+    }
+
+
 }
